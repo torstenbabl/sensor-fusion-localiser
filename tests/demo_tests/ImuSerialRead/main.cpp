9cfeba0c333ecae0b3199b7493ae2370c1731f29
Torsten Babl <torstenbabl@gmail.com>
2025-01-23 15:27:33 +0200

    Add demo tests

---
+++ tests/demo_tests/ImuSerialRead/main.cpp
@@ -0,0 +1,11 @@
+/// @brief Simple program to test the build system
+/// @author Torsten Babl
+
+#include <iostream>
+#include <libserialport.h>
+
+int main()
+{
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
---
+++ tests/pattern_examples/channels/main.cpp
@@ -0,0 +1,36 @@
+/// @brief Test passing messages between channels using a mechanism similar to a go-lng channel
+/// @author Torsten Babl
+
+#include <iostream>
+#include <thread>
+
+using std::thread, std::this_thread::sleep_for, std::cout, std::endl;
+using namespace std::chrono_literals;
+
+void functionOne()
+{
+  sleep_for(10ms);
+  cout << "threadOne: Message" << endl;
+
+}
+
+void functionTwo()
+{
+  sleep_for(20ms);
+  cout << "threadTwo: Message" << endl;
+}
+
+int main()
+{
+  cout << "main: Enter main loop" << endl;
+
+  thread threadOne(functionOne);
+  thread threadTwo(functionTwo);
+
+  cout << "main: Waiting for threads to join" << endl;
+  threadOne.join();
+  threadTwo.join();
+  cout << "main: All done, exiting" << endl;
+
+  return 0;
+}
